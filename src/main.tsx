a0e5167593ac921a1a96801be2f88246331bab44
h0915795711-hub <h0915795711@gmail.com>
2026-03-10 21:01:37 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
+import AdminPanel from './AdminPanel.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const isAdmin = window.location.pathname === '/admin'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {isAdmin ? <AdminPanel /> : <App />}
+  </StrictMode>,
 )
